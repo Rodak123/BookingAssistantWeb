78249037e78038dc1033251ec0926c49e9bd2384
Peprnro22 <peprnro22@sps-prosek.cz>
2025-09-12 09:48:32 +0200

    MyNumbers + SelectNumber

--- Models/PhoneNumber.cs
+++ Models/PhoneNumber.cs
@@ -3,11 +3,20 @@ namespace BookingAssistantWeb.Models
     public class PhoneNumber
     {
         public int Id { get; set; }
-        public string Value { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public int? UserId { get; set; }
+        public User? User { get; set; }
     }
 
     public class PhoneNumbersResponse
     {
         public List<PhoneNumber> PhoneNumbers { get; set; } = new();
+    }
+    
+     public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
     }
 }
