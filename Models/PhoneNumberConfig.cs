56194dfae00af1ad1ad3dcbc514e59af981c512f
Radek Titěra <rodak.titera@gmail.com>
2025-09-17 09:05:47 +0200

    add: ChangeNumberConfig page

--- Models/PhoneNumberConfig.cs
+++ Models/PhoneNumberConfig.cs
@@ -6,5 +6,9 @@ namespace BookingAssistantWeb.Models
         public int bookingWindowSeconds { get; set; }
         public int bookingMarginSeconds { get; set; }
         public int bookingIntervalSeconds { get; set; }
+
+        public TimeSpan BookingWindow => new(0, 0, bookingWindowSeconds);
+        public TimeSpan BookingMargin => new(0, 0, bookingMarginSeconds);
+        public TimeSpan BookingInterval => new(0, 0, bookingIntervalSeconds);
     }
 }
